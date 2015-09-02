d78266a2020a35bcfba2b4dbce3719cc9b60012c
zvdifo <xiaoxinquwenwen33@gmail.com>
2015-09-02 07:44:24 -0700

    homework56(finished)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,14 @@
 class ProductsController < ApplicationController
+	def create
+		@product = Product.new (product_params)
+		@product.save
+		
+	end
 
 	#whitelists strong-parameters moved from models to controllers
   private
-  def product_params
-    params.require(:product).permit(:name, :price)
-  end
+
+	  def product_params
+	    params.require(:product).permit(:name, :price)
+	  end
 end
